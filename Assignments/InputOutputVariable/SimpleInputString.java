b0fcf008916f6c1cf312975c1084daca601d53cf
Bharat Rav <75756788+BharatRav@users.noreply.github.com>
2022-07-28 02:33:48 +0530

    Create SimpleInputString.java

---
+++ Assignments/InputOutputVariable/SimpleInputString.java
@@ -0,0 +1,10 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+class Main {
+	public static void main (String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String S=sc.nextLine();
+		System.out.println(S);
+	}
+}
