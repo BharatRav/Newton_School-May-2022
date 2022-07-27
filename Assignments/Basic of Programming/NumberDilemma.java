7718026d535f95b7d224ef42b4bfd2a9bc61b328
Bharat Rav <75756788+BharatRav@users.noreply.github.com>
2022-07-27 17:43:48 +0530

    Create NumberDilemma.java

---
+++ Assignments/Basic of Programming/NumberDilemma.java
@@ -0,0 +1,13 @@
+static int StringToInt(String S){
+//Enter your code here
+int result=0;
+result=Integer.parseInt(S);
+return result;
+}
+
+static String IntToString(int N){
+//Enter your code here
+String str;
+str= Integer.toString(N);
+return str;
+}
