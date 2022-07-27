87e2a872206b4f10371f7c7fb6b0381e8b7da013
Bharat Rav <75756788+BharatRav@users.noreply.github.com>
2022-07-28 02:32:41 +0530

    Create SimpleSum.java

---
+++ Assignments/InputOutputVariable/SimpleSum.java
@@ -0,0 +1,14 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+class Main {
+	public static void main (String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int A= sc.nextInt();
+		int B= sc.nextInt();
+		int C= sc.nextInt();
+		int sum= A+B+C;
+		System.out.println(sum);
+
+	}
+}
