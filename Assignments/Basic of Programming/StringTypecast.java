c8e526744b8dbaf17c4bd5ca11a88385a67cca0e
Bharat Rav <75756788+BharatRav@users.noreply.github.com>
2022-07-27 17:38:04 +0530

    Create StringTypecast.java
    to run this code in IDE change class name same as file name

---
+++ Assignments/Basic of Programming/StringTypecast.java
@@ -0,0 +1,8 @@
+static String checkConevrtion(int a)
+ {    
+      //  Scanner sc = new Scanner(System.in);
+      //  int a= sc.nextInt();
+       String result=Integer.toString(a);
+       return result;
+       // your code here
+    }
