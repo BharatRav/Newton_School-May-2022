2e73daf66fb571dc383da4aeba888382d5920b5b
Bharat Rav <75756788+BharatRav@users.noreply.github.com>
2022-08-12 23:42:40 +0530

    Create MaximumValueOfX.java

---
+++ Assignments/Contest Question 09 July(May)/MaximumValueOfX.java
@@ -0,0 +1,22 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        long sum =0;
+        for (int i=n-1;i>=n-k;i--) {
+            sum= sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
