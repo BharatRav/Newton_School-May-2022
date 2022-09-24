1d224c930d7324d82d78756dac6280d751a93d8e
Bharat Rav <75756788+BharatRav@users.noreply.github.com>
2022-09-24 19:59:51 +0530

    Update SequenceFinding.java

--- QuestionOfTheDay/24 Sep/SequenceFinding.java
+++ QuestionOfTheDay/24 Sep/SequenceFinding.java
@@ -28,3 +28,35 @@ class Main {
         }
     }
 }
+
+/*
+Sequence Finding
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+You have an empty sequence A. Along with that you are given an array B of size N. You create a new integer i = 0 and perform N operations on the sequence A in the following way:
+
+Check if i == N + 1, break.
+
+Append Bi to the end of A and increment i by 1.
+
+Reverse the sequence A.
+
+Find the sequence A after all the operations.
+Input
+The first line of the input contains a single integer N.
+The second line of the input contains N space seperated integers.
+
+Constraints:
+1 <= N <= 105
+1 <= Bi <= 109
+Output
+Print the sequence A after all the operations.
+Example
+Sample Input:
+4
+1 2 3 4
+
+Sample Output:
+4 2 1 3
+*/
