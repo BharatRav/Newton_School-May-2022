f6e1aa6a06ece469f08d3044719adeaa6200ceb5
Bharat Rav <75756788+BharatRav@users.noreply.github.com>
2022-10-11 15:10:24 +0530

    Update DetectCycleInAnUndirectedGraph.java

--- Advanced Batch Slytherin/Graph/DetectCycleInAnUndirectedGraph.java
+++ Advanced Batch Slytherin/Graph/DetectCycleInAnUndirectedGraph.java
@@ -1,5 +1,8 @@
 import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
+/*
+Question Link:  https://my.newtonschool.co/playground/code/rdf9tkwgob1d/
+*/
 
 // don't change the name of this class
 // you can add inner classes if needed
@@ -86,3 +89,48 @@ class Main {
         //0 if true there is a cycle
     }
 }
+
+/*
+Detect cycle in an undirected graph
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given an undirected graph, the task is to create a program to detect if there is a cycle in the undirected graph or not.
+Input
+The first line of input contains two integers N and M which denotes the no of vertices and no of edges in the graph respectively.
+Next M lines contain space-separated integers u and v denoting that there is a directed edge from u to v.
+
+Constraints:
+1 <= N, M <= 1000
+0 <= u, v <= N-1, u != v
+There are no self loops or multiple edges.
+Output
+Print 'Yes' if there is a cycle in the graph otherwise print 'No'
+Example
+Sample Input 1:
+4 5
+0 1
+1 2
+2 3
+3 0
+0 2
+
+Sample Output 1:
+Yes
+
+Explanation:
+There is a cycle with nodes 0, 1, 2, 3
+
+Sample Input 2:
+4 3
+0 1
+1 2
+2 3
+
+Sample Output 2:
+No
+
+Explanation:
+There is no cycle in this graph
+*/
